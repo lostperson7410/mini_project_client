d5d465d8b6f4a88496e5f244f822f16c025170e1
lostperson7410 <lostperson7410@gmail.com>
2020-03-21 00:46:15 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './component/images/Cather1.jpg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'reactstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  console.log(logo);
+  return ( //ใส่หน้าบ้าน
+    <div>
+
+        <Container fluid>
+          <Row className ="colorerow">
+              <img src = {logo} alt="Logo" className = "setlogo" />
+            <Col className ="ColortCol">
+              
+
+              
+            </Col>
+            
+
+          </Row>
+        </Container>
+  
+  
     </div>
+
   );
 }
 
